c1912221ededd0c215986865cedd0528f67d21ba
Thiago Retondar <t.retondar@gmail.com>
2016-11-15 17:04:08 -0200

    First commit

---
+++ src/main/java/fei/tcc/ParentalcontrolDashboardApplication.java
@@ -0,0 +1,12 @@
+package fei.tcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentalcontrolDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParentalcontrolDashboardApplication.class, args);
+	}
+}
